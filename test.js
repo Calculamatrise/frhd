42abe5630ee0708c3e0e21e6f57c5f66bd93834c
Calculus <51772836+Calculamatrise@users.noreply.github.com>
2021-08-22 03:36:35 -0700

    Add files via upload

--- test.js
+++ test.js
@@ -3,13 +3,17 @@ import frhd, { Client, Track } from "./src/bootstrap.js";
 const client = new Client();
 
 client.on("ready", async function() {
-    const track = new Track();
-
-    track.beginPath();
-    track.moveTo(0, 0);
-    track.lineTo(10, 10, 20, 50);
-
-    console.log(track.code)
+    //this.tracks.fetch(1001).then(console.log)
+    //this.users.fetch("char").then(console.log)
+    // this.getUser("Char").then(console.log)
+    
+    for (let t_id = 208172; t_id < 800000; t_id++) {
+        await this.tracks.fetch(t_id).then(function(track) {
+            return track.vote(1);
+        }).then(function(response) {
+            return console.log(t_id, response.result || response.msg);
+        }).catch(console.error);
+    }
 });
 
 client.login("TOKEN");
