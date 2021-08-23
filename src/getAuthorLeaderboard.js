fdb7e20ef1dd29835ea61cb6c81236a22d4e2a0f
Calculus <51772836+Calculamatrise@users.noreply.github.com>
2021-08-22 21:37:46 -0700

    Delete src directory

--- src/getAuthorLeaderboard.js
+++
@@ -1,5 +0,0 @@
-import RequestHandler from "./utils/RequestHandler.js";
-
-export default function(page, callback = t => t) {
-    return RequestHandler.ajax(`/leaderboards/author/lifetime/${page}?ajax=!0`).then(callback);
-}
--- src/getPlayerLeaderboard.js
+++
@@ -1,5 +0,0 @@
-import RequestHandler from "./utils/RequestHandler.js";
-
-export default async function(page, callback = t => t) {
-    return await RequestHandler.ajax(`/leaderboards/player/lifetime/${page}?ajax=!0`).then(callback);
-}
